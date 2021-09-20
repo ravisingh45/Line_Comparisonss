0d854cc952e8ffba59d5b77a61a96b283b15df5c
ravisingh45@gmail.com <81140145+ravisingh45@users.noreply.github.com>
2021-09-20 21:20:31 +0530

    ADDED:checking two lines are equal are equal or not

--- src/main/com/bridgelabz/LineComparison.java
+++ src/main/com/bridgelabz/LineComparison.java
@@ -4,18 +4,37 @@ import java.util.Scanner;
 
 public class LineComparison {
     public static void main(String[] args) {
-        int comp;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("enter the value of X1:");
-        int x1 = scanner.nextInt();
-        System.out.println("enter the value of X2:");
-        int x2 = scanner.nextInt();
-        System.out.println("enter the value of Y1:");
-        int y1 = scanner.nextInt();
-        System.out.println("enter the value of Y2:");
-        int y2 = scanner.nextInt();
-        comp = ((x2 -x1)*2*2+(y2-y1)*2*2);
-        System.out.println("length of the line is : "+ comp);
+        System.out.println("enter the value of x1,x2,y1,y2");
+        int coor_x1 = scanner.nextInt();
+        int coor_y1 = scanner.nextInt();
+        int coor_x2 = scanner.nextInt();
+        int coor_y2 = scanner.nextInt();
+        System.out.println("enter x1: "+coor_x1);
+        System.out.println("enter y1: "+ coor_y1);
+        System.out.println("enter x2: "+coor_x2);
+        System.out.println("enter y2: "+coor_x1);
+        int length1 = (int)(Math.sqrt((coor_x2-coor_x1)^2+(coor_y2-coor_y1)^2));
+        System.out.println("length1 of the line is : "+ length1);
 
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("enter the value of X1,X2,Y1,Y2");
+        int Coor_X1 =scanner1.nextInt();
+        int Coor_Y1 = scanner1.nextInt();
+        int Coor_X2 = scanner1.nextInt();
+        int Coor_Y2 = scanner1.nextInt();
+        System.out.println("enter x1: "+Coor_X1);
+        System.out.println("enter y1: "+Coor_X2);
+        System.out.println("enter x2: "+Coor_Y1);
+        System.out.println("enter y2: "+Coor_Y2);
+       int length2 = (int)(Math.sqrt((Coor_X2-Coor_X1)^2+(Coor_Y2-Coor_Y1)^2));
+        System.out.println("length2 of the line is: " + length2);
+        if (length1 == length2){
+            System.out.println("Both line is equal ");
+        }
+        else
+        {
+            System.out.println("both line is not equal");
+        }
     }
 }
