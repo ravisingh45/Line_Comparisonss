af0694c87fd18285319aaabd43c5aba6d1bba71b
ravisingh45@gmail.com <81140145+ravisingh45@users.noreply.github.com>
2021-09-20 21:44:49 +0530

    ADDED:compareing two lines are  equal or greater or lesser

--- src/main/com/bridgelabz/LineComparison.java
+++ src/main/com/bridgelabz/LineComparison.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class LineComparison {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("enter the value of x1,x2,y1,y2");
+        System.out.println("enter the value of x1,y1,x2,y2");
         int coor_x1 = scanner.nextInt();
         int coor_y1 = scanner.nextInt();
         int coor_x2 = scanner.nextInt();
@@ -13,7 +13,7 @@ public class LineComparison {
         System.out.println("enter x1: "+coor_x1);
         System.out.println("enter y1: "+ coor_y1);
         System.out.println("enter x2: "+coor_x2);
-        System.out.println("enter y2: "+coor_x1);
+        System.out.println("enter y2: "+coor_y1);
         int length1 = (int)(Math.sqrt((coor_x2-coor_x1)^2+(coor_y2-coor_y1)^2));
         System.out.println("length1 of the line is : "+ length1);
 
@@ -30,11 +30,15 @@ public class LineComparison {
        int length2 = (int)(Math.sqrt((Coor_X2-Coor_X1)^2+(Coor_Y2-Coor_Y1)^2));
         System.out.println("length2 of the line is: " + length2);
         if (length1 == length2){
-            System.out.println("Both line is equal ");
+            System.out.println("Both line are equal ");
         }
-        else
+        else if (length1 <= length2){
+            System.out.println("line1 is small then line2");
+        }
+        else if (length2 <= length1)
         {
-            System.out.println("both line is not equal");
+            System.out.println("line1 is greater then line2");
         }
+
     }
 }
